a990ab82a918b0ecf0eb4131b170eb5c9bb2dd49
admin <qwe>
2021-06-20 00:40:46 +0300

    MpInstance wip

---
+++ JET/MPInstance.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Networking;
+
+namespace JET
+{
+#pragma warning disable 618
+    public class MpInstance : NetworkManager
+    {
+        public static int NextChannelId = 5;
+        public const int Port = 5000;
+
+        public ulong LocalIndex { get; private set; }
+        public double LocalTime { get; private set; }
+        public bool RaidStarted { get; private set; }
+        public bool WorldSpawned { get; private set; }
+
+        //public readonly ConcurrentDictionary<int, EFT.NetworkGameSession> GameSessions = new ConcurrentDictionary<int, EFT.NetworkGameSession>();
+        //public readonly ConcurrentDictionary<int, ServerPlayer> NetworkClients = new ConcurrentDictionary<int, ServerPlayer>();
+
+        public void FixedUpdate()
+        {
+        }
+    }
+}
