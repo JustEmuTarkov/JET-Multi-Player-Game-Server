a1541f415e484c88a30bb357d8b2c362080ca536
admin <qwe>
2021-06-20 02:45:01 +0300

    ServerHandlers wip

---
+++ JET/Server/Handlers/ServerHandlers.cs
@@ -0,0 +1,19 @@
+﻿using JET.Server.Messages;
+using UnityEngine.Networking;
+
+#pragma warning disable 618
+
+namespace JET.Server.Handlers
+{
+    public class ServerHandlers
+    {
+        public static void RegisterServerHandlers()
+        {
+        }
+
+        private static void RegisterAuthHandler()
+        {
+            NetworkServer.RegisterHandler(AuthRequestMessage.MessageID, AuthHandlers.OnAuthMessage);
+        }
+    }
+}
