b50d283a9a0b822018fd385d23a81848f2354b7c
admin <qwe>
2021-06-20 02:46:02 +0300

    wip auth message handler

---
+++ JET/Server/Handlers/AuthHandlers.cs
@@ -0,0 +1,24 @@
+﻿using Comfort.Common;
+using JET.Server.Messages;
+using JET.Utilities;
+using JET.Utilities.HTTP;
+using UnityEngine.Networking;
+
+#pragma warning disable 618
+
+namespace JET.Server.Handlers
+{
+    public class AuthHandlers
+    {
+        public static void OnAuthMessage(NetworkMessage message)
+        {
+            var authRequest = message.ReadMessage<AuthRequestMessage>();
+            var server = Singleton<ServerInstance>.Instance;
+
+            var json = new Request(null, Config.BackendUrl).GetJson(
+                $"/client/game/profile/list/server/{authRequest.ProfileId}");
+
+            var channelId = ServerInstance.GetNextChannelId();
+        }
+    }
+}
