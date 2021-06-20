ffd64b399454009208383341129c26843b978ea9
admin <qwe>
2021-06-20 19:02:24 +0300

    wip player spawn message

--- JET/Server/Messages/PlayerSpawnMessage.cs
+++ JET/Server/Messages/PlayerSpawnMessage.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using ComponentAce.Compression.Libs.zlib;
 using EFT;
+using JET.Server.Player;
+using JET.Server.Trash;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -13,18 +13,28 @@ namespace JET.Server.Messages
 {
     public class PlayerSpawnMessage : MessageBase
     {
-        public int Id;
-        public byte ChannelId;
-        public Vector3 Position1;
-        public byte ChannelIndex;
-        public bool IsAlive;
-        public Vector3 Position2;
-        public Quaternion Rotation;
-        public bool IsInPronePose;
-        public float PoseLevel;
-        public GClass1715 Inventory;
-        public Profile PlayerProfile;
-
+        public int Id = -1;
+        public byte ChannelId = Byte.MinValue;
+        public Vector3 Position1 = Vector3.negativeInfinity;
+        public byte ChannelIndex = Byte.MinValue;
+        public bool IsAlive = false;
+        public Vector3 Position2 = Vector3.negativeInfinity;
+        public Quaternion Rotation = Quaternion.identity;
+        public bool IsInPronePose = false;
+        public float PoseLevel = Single.NaN;
+        public GClass1715 Inventory = new GClass1715();
+        public Profile PlayerProfile = new Profile();
+        public int IdGeneratorId = -1;
+        public int ScavExfilMask = -1;
+        public int AnimationVariant = -1;
+        public EController Econtroller = EController.Empty;
+        public bool IsInSpawnOperation = false;
+        public PlayerSpawnMessageTrash.SetupInfo MisFireInfo = new PlayerSpawnMessageTrash.SetupInfo();
+        public PlayerSpawnMessageTrash.SetupInfo JamInfo = new PlayerSpawnMessageTrash.SetupInfo();
+        public bool IsStationaryWeapon = false;
+        public string ItemId = String.Empty;
+        public string[] SearchItemIds = new string[0]; // it's always 0 length, should be...
+        public PlayerHealthController HealthController = null;
 
         public override void Serialize(NetworkWriter writer)
         {
@@ -38,20 +48,80 @@ namespace JET.Server.Messages
             writer.Write(IsInPronePose);
             writer.Write(PoseLevel);
 
+            // write inventory data
             using (var memoryStream = new MemoryStream())
             {
                 using (var binaryWriter = new BinaryWriter(memoryStream))
                 {
                     binaryWriter.Write(GClass970.SerializeInventory(Inventory));
-                    byte[] buff = memoryStream.ToArray();
+                    var buff = memoryStream.ToArray();
                     writer.WriteBytesAndSize(buff, buff.Length);
                 }
             }
 
+            // write profile data
             byte[] profileData = SimpleZlib.CompressToBytes(PlayerProfile.ToJson(), 9);
             writer.WriteBytesAndSize(profileData, profileData.Length);
 
+            writer.Write(IdGeneratorId);
+
+            if (IsAlive)
+            {
+                writer.Write(IsAlive);
+                writer.Write(ScavExfilMask);
+
+                var healthState = HealthController.SerializeState();
+                writer.WriteBytesAndSize(healthState, healthState.Length);
+
+                writer.Write(AnimationVariant);
+                writer.Write((byte) Econtroller);
+                switch (Econtroller)
+                {
+                    case EController.None:
+                        Console.WriteLine(
+                            "JET.Server.Messages.PlayerSpawnMessage.Serialize: WARNING! no hands controllers");
+                        return;
+                    case EController.Empty:
+                        break;
+                    case EController.Firearm:
+                        writer.Write(IsInSpawnOperation);
+                        MisFireInfo.Serialize(writer);
+                        JamInfo.Serialize(writer);
+                        writer.Write(IsStationaryWeapon);
+
+                        writer.Write(ItemId);
+                        break;
+                    case EController.Meds:
+                        writer.Write(ItemId);
+                        break;
+                    case EController.Grenade:
+                        writer.Write(ItemId);
+                        break;
+                    case EController.Knife:
+                        writer.Write(ItemId);
+                        break;
+                    case EController.QuickGrenade:
+                        writer.Write(ItemId);
+                        break;
+                    case EController.QuickKnife:
+                        writer.Write(ItemId);
+                        break;
+                    default:
+                        throw new Exception(
+                            "JET.Server.Messages.PlayerSpawnMessage.Serialize: Invalid EController enum value:" +
+                            Econtroller);
+                }
+
+                writer.Write((byte) SearchItemIds.Length);
+                foreach (var t in SearchItemIds)
+                {
+                    writer.Write(t);
+                }
+            }
+
             base.Serialize(writer);
         }
+
+        public const short MessageID = 155;
     }
 }
---
+++ JET/Server/Trash/PlayerSpawnMessageTrash.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.Networking;
+
+#pragma warning disable 618
+
+namespace JET.Server.Trash
+{
+    public static class PlayerSpawnMessageTrash
+    {
+        // Token: 0x060092E1 RID: 37601 RVA: 0x000FB0A7 File Offset: 0x000F92A7
+        public static void Serialize(this SetupInfo setupInfo, NetworkWriter writer)
+        {
+            writer.Write(setupInfo.Count);
+            writer.Write(setupInfo.Seed);
+            writer.Write(setupInfo.Index);
+        }
+
+        // Token: 0x060092E2 RID: 37602 RVA: 0x000FB0CD File Offset: 0x000F92CD
+        public static void Deserialize(this SetupInfo setupInfo, NetworkReader reader)
+        {
+            setupInfo.Count = reader.ReadInt32();
+            setupInfo.Seed = reader.ReadInt32();
+            setupInfo.Index = reader.ReadInt32();
+        }
+
+        public class SetupInfo
+        {
+            // Token: 0x060092DF RID: 37599 RVA: 0x000FB07A File Offset: 0x000F927A
+            public override string ToString()
+            {
+                return $"RandomsSetupInfo  Seed: {Seed}, Count: {Count}, Index: {Index}";
+            }
+
+            // Token: 0x040084A0 RID: 33952
+            public int Seed;
+
+            // Token: 0x040084A1 RID: 33953
+            public int Count;
+
+            // Token: 0x040084A2 RID: 33954
+            public int Index;
+        }
+    }
+}
