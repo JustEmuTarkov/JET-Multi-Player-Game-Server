908b111020eef163cab3914b35f32b26ff1fd2de
admin <qwe>
2021-06-21 00:43:24 +0300

    added ability for getting static method info

--- JET/Utilities/Reflection/PrivateMethodAccessor.cs
+++ JET/Utilities/Reflection/PrivateMethodAccessor.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace JET.Utilities.Reflection
@@ -14,6 +15,18 @@ namespace JET.Utilities.Reflection
         public static MethodInfo GetPrivateMethodInfo(object obj, string methodName)
         {
             MethodInfo[] methods = obj.GetType().GetMethods(PrivateValueAccessor.Flags);
+            return methods.FirstOrDefault(methodInfo => methodInfo.Name == methodName);
+        }
+        
+        /// <summary>
+        /// A static method to get the FieldInfo of a private field of any object.
+        /// </summary>
+        /// <param name="type">The type that has the private method</param>
+        /// <param name="methodName">The name of the private method</param>
+        /// <returns>MethodInfo object. It has the method name and a useful Invoke() method.</returns>
+        public static MethodInfo GetPrivateStaticMethodInfo(Type type, string methodName)
+        {
+            MethodInfo[] methods = type.GetMethods(PrivateValueAccessor.Flags);
             return methods.FirstOrDefault(methodInfo => methodInfo.Name == methodName);
         }
     }
