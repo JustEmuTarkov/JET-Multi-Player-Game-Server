ada1ee3530d60c879934797da99fb817a72e5250
admin <qwe>
2021-06-20 18:53:54 +0300

    wip player health controller

---
+++ JET/Server/Player/PlayerHealthController.cs
@@ -0,0 +1,29 @@
+﻿using EFT;
+using EFT.InventoryLogic;
+
+namespace JET.Server.Player
+{
+    public class PlayerHealthController : GClass1438
+    {
+        public PlayerHealthController(Profile.GClass1127 profileHealth, GClass1731 inventory, GClass1143 skills) : base(
+            profileHealth, inventory, skills)
+        {
+            
+        }
+
+        public override void ApplyItem(Item item, EBodyPart bodyPart, float? amount = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void CancelApplyingItem()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool _sendNetworkSyncPackets
+        {
+            get { return true; }
+        }
+    }
+}
