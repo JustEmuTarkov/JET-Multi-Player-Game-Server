d2bf61ce0ca0ce972c7f5e4779578f6919269c88
admin <qwe>
2021-06-21 00:43:40 +0300

    wip auth handler

--- JET/Server/Handlers/AuthHandlers.cs
+++ JET/Server/Handlers/AuthHandlers.cs
@@ -1,5 +1,7 @@
 ﻿using Comfort.Common;
+using EFT;
 using JET.Server.Messages;
+using JET.Server.Player;
 using JET.Utilities;
 using JET.Utilities.HTTP;
 using UnityEngine.Networking;
@@ -15,10 +17,12 @@ namespace JET.Server.Handlers
             var authRequest = message.ReadMessage<AuthRequestMessage>();
             var server = Singleton<ServerInstance>.Instance;
 
-            var json = new Request(null, Config.BackendUrl).GetJson(
-                $"/client/game/profile/list/server/{authRequest.ProfileId}");
+            var profiles = new Request(null, Config.BackendUrl).GetJson($"/client/game/profile/list/{authRequest.ProfileId}").ParseJsonTo<Profile[]>();
 
             var channelId = ServerInstance.GetNextChannelId();
+            var spawnMessage = PlayerSpawnMessage.FromProfile(channelId, profiles[0]);
+
+            var player = ServerPlayer.Create(spawnMessage.PlayerID, spawnMessage.Position, Singleton<ServerInstance>.Instance);
         }
     }
 }
