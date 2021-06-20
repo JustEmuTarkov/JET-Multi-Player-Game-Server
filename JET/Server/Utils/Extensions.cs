4c1023940ba872c36b57e9147d1baf2d0ff41fcf
admin <qwe>
2021-06-21 00:41:50 +0300

    added extension for getting inventory prefabs

---
+++ JET/Server/Utils/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using EFT;
+using EFT.InventoryLogic;
+
+namespace JET.Server.Utils
+{
+    public static class Extensions
+    {
+        public static ResourceKey[] GetAllInventoryPrefabs(this Profile profile)
+        {
+            Item[] sources = profile.Inventory.AllPlayerItems
+                .Concat(profile.Inventory.GetAllEquipmentItems()).ToArray();
+
+            return sources.SelectMany(x => x.Template.AllResources).ToArray();
+        }
+    }
+}
