759d7281d830746f83e8f045dc2f032cf8e1a848
admin <qwe>
2021-06-23 15:31:50 +0300

    wip handlers

---
+++ JET/Server/Handlers/PlayerReadyHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Comfort.Common;
+using JET.Server.Messages;
+using UnityEngine.Networking;
+
+#pragma warning disable 618
+
+namespace JET.Server.Handlers
+{
+    public static class PlayerReadyHandler
+    {
+        public static void OnPlayerReadyMessage(NetworkMessage message)
+        {
+            var someBundleMessage = new LoadBundlesMessage
+            {
+                ID = 0, Prefabs = Singleton<ServerInstance>.Instance.AllPrefabs.ToArray()
+            };
+
+            NetworkServer.SendToClient(
+                message.conn.connectionId,
+                LoadBundlesMessage.MessageID,
+                someBundleMessage
+            );
+
+            Console.WriteLine($"OnPlayerReady received, conn id is: {message.conn.connectionId}");
+        }
+    }
+}
