b88c56ce3de63c97a44040e0fe91b57ffa3f11a8
admin <qwe>
2021-06-25 21:52:20 +0300

    added GameUtils added getter for map points

---
+++ JET/Server/Utils/GameUtils.cs
@@ -0,0 +1,12 @@
+﻿using EFT.UI.Map;
+
+namespace JET.Server.Utils
+{
+    public class GameUtils
+    {
+        public static MapPoints GetMapPoints(ESideType sideType, string locationId)
+        {
+            return GClass1376.Pop<MapPoints>("MapPointConfigs/" + locationId + (sideType == ESideType.Savage ? "_scav" : string.Empty));
+        }
+    }
+}
